54712fe2ac7cf79193ee0fde8d4e637a3c9e40c9
youmewmew <n0ne777@yandex.ru>
2022-08-10 20:46:53 +0500

    realize useState on button favorite

--- crtreactjs/src/App.js
+++ crtreactjs/src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
+import React from 'react';
+import AddButtonItemCart from './components/AddButtonItemCart.js';
 // import { useState } from 'react';
  
 
-const ItemCart = ({itemImageSrc, itemName, itemRatingValue, itemCommentAmount, itemPriceCurrent, itemOldPrice, itemBonusAmount, itemAddBtnBacket, itemAddBtnFavorite, itemAddBtnCompare, descriptionImage, iconDescription }) => {
+const ItemCart = ({itemImageSrc, itemName, itemRatingValue, itemCommentAmount, itemPriceCurrent, itemOldPrice, itemBonusAmount, descriptionImage }) => {
   // тут я попыталась сделать UseState
-  // const [addFavorite, setAddFavorite] = useState(false);
-  // const addFavoriteBtn = () => {
-  //   setAddFavorite(true);
-  // }
-  // const removeFavoriteBtn = () => {
-  //   setAddFavorite(false);
-  // }
 
   return (
     <div className="itemCart">
@@ -23,37 +18,18 @@ const ItemCart = ({itemImageSrc, itemName, itemRatingValue, itemCommentAmount, i
       {/* блок отзывы и рейтинг */}
       <div className="itemCart-rating-wrapper">
         <a className="itemCart-rating__value value-link" href="/">{itemRatingValue}</a>
-        <a className="itemCart-rating__comment-amount comment-amount-link" href="/">{itemCommentAmount}{' '}отзывов</a>
+        <a className="itemCart-rating__comment-amount comment-amount-link" href="/">{itemCommentAmount} отзывов</a>
       </div>
       {/* блок с ценами */}
       <div className="itemCart-price-wrapper">
-        <span className="itemCart-price__current">{itemPriceCurrent}{' '}Р</span>
+        <span className="itemCart-price__current">{itemPriceCurrent} Р</span>
         <span className="itemCart-price__old">{itemOldPrice}</span>
       </div>
 
-      <span className="itemCart-bonus-amount">+{itemBonusAmount}{' '}
-        <span className="itemCart-bonus-amount__addition">Бонусных рублей</span>
+      <span className="itemCart-bonus-amount">+{itemBonusAmount}
+      <span className="itemCart-bonus-amount__addition"> Бонусных рублей</span>
       </span>
-      {/* блок с кнопками */}
-      <div className="itemCart-addBtn-wrapper">
-          {/* кнопка добавить в корзину */}
-        <button className="itemCart-addBtn addBtn-backet" type="button">
-          <img src={"/image/icon/basket-icon.svg"} alt={"Добавить"}></img>
-          {itemAddBtnBacket}
-        </button>
-
-        {/* кнопка добавить в избранное */}
-        <button className="itemCart-addBtn addBtn-favorite" type="button" >
-          <img src={"/image/icon/icon-favorite.svg"} alt={"Добавить"}></img>
-          {itemAddBtnFavorite}
-        </button>
-
-        {/* кнопка "добавить в сравнение" */}
-        <button className="itemCart-addBtn addBtn-compare"  type="button">
-          <img src={"/image/icon/compare.svg"} alt={"Добавить"}></img>
-          {itemAddBtnCompare}
-        </button>
-      </div>
+      <AddButtonItemCart />
     </div>
   )
 };
@@ -70,11 +46,9 @@ function App() {
       itemPriceCurrent="69 999"
       itemOldPrice="79 999"
       itemBonusAmount="2 100"
-      itemAddBtnBacket
-      itemAddBtnFavorite
-      itemAddBtnCompare
       >
       </ItemCart> 
+
       <ItemCart 
       itemImageSrc='/image/desctopRombica.webp'
       descriptionImage="Монитор Rombica SkyView"
@@ -84,11 +58,9 @@ function App() {
       itemPriceCurrent="7 899"
       itemOldPrice="11 799"
       itemBonusAmount="237"
-      itemAddBtnBacket
-      itemAddBtnFavorite
-      itemAddBtnCompare
       >
       </ItemCart> 
+
       <ItemCart 
       itemImageSrc='/image/smartHuawei.webp'
       descriptionImage="Смартфон Huawei nova"
@@ -98,11 +70,9 @@ function App() {
       itemPriceCurrent="15 999"
       itemOldPrice="18 999"
       itemBonusAmount="480"
-      itemAddBtnBacket
-      itemAddBtnFavorite
-      itemAddBtnCompare
       >
       </ItemCart> 
+
       <ItemCart 
       itemImageSrc='/image/TVToshiba.webp'
       descriptionImage="Телевизор Toshiba"
@@ -112,9 +82,6 @@ function App() {
       itemPriceCurrent="29 999"
       itemOldPrice="39 999"
       itemBonusAmount="900"
-      itemAddBtnBacket
-      itemAddBtnFavorite
-      itemAddBtnCompare
       >
       </ItemCart> 
     </div>
